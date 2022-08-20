282d65a118948be7ced949c92c42546e506dc925
Matt Young <71722219+mattyoung7@users.noreply.github.com>
2022-08-20 15:14:08 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const navBtn = document.querySelector('#nav-btn');
 const navList = document.querySelector('#navlist');
 const navLinks = document.querySelectorAll('.nav-link');
 
+const hello = document.querySelector('.hello');
+const title = document.querySelector('.title');
+const subtitle = document.querySelector('.subtitle');
+const icons = document.querySelector('.icons');
+const scroll = document.querySelector('.scroll');
+
 navBtn.addEventListener('click', () => {
     const visibility = navList.getAttribute('data-visible');
     if (visibility === 'false') {
@@ -10,7 +16,7 @@ navBtn.addEventListener('click', () => {
     } else {
         navList.setAttribute('data-visible', 'false');
         setTimeout(() => {
-            navBtn.style.color='#00BDAA';
+            navBtn.style.color='#FF2ED9';
         }, 300);
     }
 })
@@ -23,7 +29,27 @@ for(let navLink of navLinks){
     navLink.addEventListener('click', ()=> {
         navList.setAttribute('data-visible', 'false');
         setTimeout(() => {
-            navBtn.style.color='#00BDAA';
+            navBtn.style.color='#FF2ED9';
         }, 300);
     })
 }
+
+setTimeout(()=> {
+    hello.style.opacity='1';
+}, 2200);
+
+setTimeout(()=> {
+    title.style.opacity='1';
+}, 3200);
+
+setTimeout(()=> {
+    subtitle.style.opacity='1';
+}, 3700);
+
+setTimeout(()=> {
+    icons.style.opacity='1';
+}, 3700);
+
+setTimeout(()=> {
+    scroll.style.opacity='1';
+}, 5000);
